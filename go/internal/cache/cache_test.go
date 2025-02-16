a9633aef9c7f75ad7cede1b34c281734f1b75232
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2025-02-15 22:28:30 -0500

    test: mru policy functional

--- go/internal/cache/cache_test.go
+++ go/internal/cache/cache_test.go
@@ -59,3 +59,20 @@ func TestLruEvictionIncreasedCapacity(t *testing.T) {
 	require.NoError(t, err)
 	require.Nil(t, record)
 }
+
+func TestMruEviction(t *testing.T) {
+	c := newCache(2)
+	defaultOpts, _ := getOptions()
+	defaultOpts.evictOnGet = false
+	defaultOpts.evictionPolicy = EvictMRU
+
+	require.NoError(t, c.Set("user:1", []byte("user1"), defaultOpts))
+	require.NoError(t, c.Set("user:2", []byte("user2"), defaultOpts))
+	record, err := c.Get("user:1", defaultOpts)
+	require.NoError(t, err)
+	require.Equal(t, []byte("user1"), record)
+	require.NoError(t, c.Set("user:3", []byte("user3"), defaultOpts))
+	record, err = c.Get("user:1", defaultOpts)
+	require.NoError(t, err)
+	require.Nil(t, record)
+}
